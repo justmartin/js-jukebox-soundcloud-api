f4c1745ce3fdfa906ff029eb737f09ffddc32347
justmartin <justin.lamar.martin@gmail.com>
2016-09-22 14:12:10 -0700

    Added SoundCloud SDK. Looped in SC stream functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,85 @@
-// Your JavaScript Jukebox should:
-// Display at least one song on the page when the page loads
+// Your SoundCloud enabled jukebox should allow the user to do the following things:
 
-// Give the user the ability to play that song, without using the "built-in" play button. This could be through a 
-// different button, through clicking or mousing over an image on the page, or any other device of your choosing.
+// Play a track off of SoundCloud based on its track ID
+// Pause the currently playing track
+// Display the following current track information:
+// Artist name with link to his/her profile page
+// Title with link to track's page
+// Description, genre and release date
+// Artwork
 
-// Give the user the ability to stop that song without using the "built-in" stop button. Once again, this could be 
-// through a different button, through clicking or mousing over an image on the page, or any other device of your 
-// choosing.
+// Playlist Challenge:
+// If you have time and are looking for an extra challenge try the following. Create a playlist on 
+// SoundCloud and enable the user to do the following things:
 
-// Give the user the ability to load at least one different song into the Jukebox besides the one that is loaded when 
-// the page initially renders
-
-// The whole Jukebox should be backed by an object called Jukebox with methods to play, stop, and load songs.
+// Play a track off of a chosen playlist
+// Pause the currently playing track
+// Skip to the next or previous track
+// Jump to the beginning when skipping forward from the last track
+// Jump to the end when skipping backward the first track
+// Display the following playlist information:
+// Creator with link to his/her profile page
+// Playlist title with link to playlist page
+// Artwork
 
 $(document).ready(function() {
 
-	//defines new Jukebox variable
-    jukebox = new Jukebox();
+	SC.initialize({
+	  client_id: "9a1358c581fbce53ea61cd78cd57768f"
+	});
 
-   	//defines new songs and adds them to songs array via Jukebox functions
-	var song1 = new Song("assets/audio/Hall-&-Oates-Sara-Smile.mp3");
-	var song2 = new Song("assets/audio/The-Commodores-Easy.mp3")
-	jukebox.addSong(song1);
-	jukebox.addSong(song2);
+	$("#search-sc-button").click(function() {
+		artist = $("#artist-input").val();
+		SC.get('/tracks', {q: artist}).then(function(tracks) {
+			i = 0;
+			tracklist = tracks;
+			console.log(tracklist);
+		});
+	});
 
-	//call Jukebox functions based on corresponding button click using jQuery
 	$("#play-button").click(function() {
-		jukebox.playSong();
+		SC.stream('/tracks/' + tracklist[i].id).then(function(player){
+	  		player.play();
+	  		assign_information(i);
+		});
 	});
 
 	$("#stop-button").click(function() {
-		jukebox.stopSong();
+		SC.stream('/tracks/' + tracklist[i].id).then(function(player){
+	  		player.pause();
+		});
 	});
 
 	$("#next-button").click(function() {
-		jukebox.nextSong();
+		var arrayLength = tracklist.length; //defines array length
+		for (var i = 0; i < arrayLength; i++) { //handling ability to only loop till end of array
+		    	SC.stream('/tracks/' + tracklist[i].id).then(function(player){
+	  			player.play();
+	  			assign_information(i);
+			});
+		};
 	});
 
 	$("#back-button").click(function() {
-		jukebox.previousSong();
-	});
-
-	$("#volume-down").click(function() {
-		jukebox.turnVolumeDown();
-	});
-
-	$("#volume-up").click(function() {
-		jukebox.turnVolumeUp();
+		var arrayLength = tracklist.length; //defines array length
+		for (var i = 0; i < arrayLength; i--) { //handling ability to only loop till end of array
+		    	SC.stream('/tracks/' + tracklist[i].id).then(function(player){
+	  			player.play();
+			});
+		};
 	});
 
 });
 
-//creates a Song object that takes a source. will be used to populate songs array.
-function Song(source) {
-	this.source = source;
-};
+assign_information = function(i) {
+	$("#artist").append(artist);
+  	$("#profile").append(tracklist[i].permalink_url);
+  	$("#title").append(tracklist[i].title);
+  	$("#description").append(tracklist[i].description);
+  	$("#genre").append(tracklist[i].genre);
+  	$("#release").append(tracklist[i].release_year);
+  	$("#album-artwork").attr("src", tracklist[i].artwork_url);
+}
 
-//defines Jukebox object
-function Jukebox() {
-	i = 0; //defines interator
-	this.songs = []; //defines array to store song sources
 
-	this.addSong = function(song) {
-		this.songs.push(song); //adds sources to songs array
-		$("#audio-player").attr("src", this.songs[0].source); //assigns first song in array to audio source on load 
-	};
 
-	this.playSong = function() {
-		$("#audio-player")[0].play(); //plays current indexed song
-	};
-
-	this.stopSong = function() {
-		$("#audio-player")[0].pause(); //stops current indexed song
-	};
-
-	this.nextSong = function(songs) {
-		var arrayLength = this.songs.length; //defines array length
-		for (var i = 0; i < arrayLength; i++) { //handling ability to only loop till end of array
-		    $("#audio-player")[0].src = this.songs[i].source; //assigns source of audio tag based on array index
-			this.playSong(); //plays current indexed song
-		}
-	};
-
-	this.previousSong = function() {
-		var arrayLength = this.songs.length; //defines array length
-		for (var i = 0; i < arrayLength; i--) { //handling ability to only loop till beginning of array
-		    $("#audio-player")[0].src = this.songs[i].source; //assigns source of audio tag based on array index
-			this.playSong(); //plays current indexed song
-		}
-	};
-
-	this.turnVolumeDown = function() {
-		$("#audio-player")[0].volume-=0.1; //decreases volume
-	};
-
-	this.turnVolumeUp = function() {
-		$("#audio-player")[0].volume+=0.1; //increases volume
-	};
-};
